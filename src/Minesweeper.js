b3f96f7418a2adf0ce8cb05f169376b64e96b98e
Chris <chrispackett1998@gmail.com>
2018-08-18 18:53:08 -0400

    added timer, fixed checkBox bug by adding POST headers, new icons

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 const BASE_URL = 'https://minesweeper-api.herokuapp.com'
+const START_TIME = Date.now()
 
 class Minesweeper extends Component {
     constructor(props) {
@@ -9,11 +10,13 @@ class Minesweeper extends Component {
             game: {
                 board: []
             },
-            gameId: ''
+            gameId: '',
+            timer: 0
         }
     }
 
     componentDidMount() {
+        this.startTimer()
         fetch(`${BASE_URL}/games/`, {
             method: "POST",
             body: JSON.stringify({
@@ -28,6 +31,18 @@ class Minesweeper extends Component {
                 gameId: newGame.id
             })
         })
+    }
+
+    startTimer() {
+        this.timerID = setInterval(() => {
+            this.setState({
+                timer: Math.floor((Date.now() - START_TIME) / 1000)
+            })
+        }, 1000)
+    }
+
+    stopTimer() {
+        clearInterval(this.timerID)
     }
 
     checkBox = (row, col) => {
@@ -47,11 +62,21 @@ class Minesweeper extends Component {
             this.setState({
                 game: newGame
             })
+            if (this.state.game.state === "lost") {
+                console.log('you lost!')
+                this.stopTimer()
+            }
+            else if (this.state.game.state === "won") {
+                console.log('you won!')
+                this.stopTimer()
+            }
         })
         .catch(console.error)
+
     }
 
-    flagBox = (row, col) => {
+    flagBox = (e, row, col) => {
+        e.preventDefault()
         fetch(`${BASE_URL}/games/${this.state.gameId}/flag`, {
             method: "POST",
             headers: {
@@ -72,8 +97,14 @@ class Minesweeper extends Component {
     }
 
     renderCells = (row, col) => {
-        if (this.state.game.board[row][col] === " ") {
-            return "U"
+        if (this.state.game.board[row][col] === "_") {
+            return "◻️"
+        }
+        else if (this.state.game.board[row][col] === "F") {
+            return "🚩"
+        }
+        else if (this.state.game.board[row][col] === "*") {
+            return "💣"
         }
         else {
             return this.state.game.board[row][col]
@@ -86,6 +117,9 @@ class Minesweeper extends Component {
                 <div className='title'>
                     Minesweeper Game ID: #{this.state.game.id}
                 </div>
+                <div>
+                    It's been {this.state.timer} seconds since you started!
+                </div>
                 <div className='board-border'>
                     {this.state.game.board.map((row, i) => {
                         return (
@@ -96,7 +130,7 @@ class Minesweeper extends Component {
                                         key={j} 
                                         className='col' 
                                         onClick={() => this.checkBox(i, j)} 
-                                        onContextMenu={() => this.flagBox(i, j)}>
+                                        onContextMenu={(e) => this.flagBox(e, i, j)}>
                                             {this.renderCells(i, j)}
                                         </span>
                                     )
