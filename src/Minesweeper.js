68efa288b07e8441a202d5bae6dc6ff3b4ef6831
Chris <chrispackett1998@gmail.com>
2018-08-18 21:08:16 -0400

    refactored some, also added won/lost result

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react';
 
-const BASE_URL = 'https://minesweeper-api.herokuapp.com'
+const BASE_URL = 'https://minesweeper-api.herokuapp.com/games/'
 const START_TIME = Date.now()
 
 class Minesweeper extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            game: {
-                board: []
-            },
+            game: { board: [] }, 
             gameId: '',
-            timer: 0
+            timer: 0, // in seconds
+            result: ''
         }
-    }
-
-    componentDidMount() {
-        this.startTimer()
-        fetch(`${BASE_URL}/games/`, {
-            method: "POST",
-            body: JSON.stringify({
-                difficulty: 0
-            })
-        })
-        .then(resp => resp.json())
-        .then(newGame => {
-            console.log(newGame)
-            this.setState({
-                game: newGame,
-                gameId: newGame.id
-            })
-        })
     }
 
     startTimer() {
         this.timerID = setInterval(() => {
-            this.setState({
-                timer: Math.floor((Date.now() - START_TIME) / 1000)
-            })
+            this.setState({ timer: Math.floor((Date.now() - START_TIME) / 1000) })
         }, 1000)
     }
 
@@ -45,12 +24,37 @@ class Minesweeper extends Component {
         clearInterval(this.timerID)
     }
 
-    checkBox = (row, col) => {
-        fetch(`${BASE_URL}/games/${this.state.gameId}/check`, {
+    componentDidMount() {
+        this.startTimer()
+        fetch(`${BASE_URL}`, {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-            },
+            headers: { "Content-Type": "application/json; charset=utf-8" },
+            body: JSON.stringify({ difficulty: 0 })
+        })
+        .then(resp => resp.json())
+        .then(newGame => {
+            this.setState({
+                game: newGame,
+                gameId: newGame.id
+            })
+        })
+    }
+
+    displayGameResult() {
+        if (this.state.game.state === "won") {
+            this.setState({ result: `Good job, you won in ${this.state.timer} seconds!` })
+            this.stopTimer()
+        }
+        else if (this.state.game.state === "lost") {
+            this.setState({ result: 'Oh no... you lose!' })
+            this.stopTimer()
+        }
+    }
+
+    checkBox = (row, col) => {
+        fetch(`${BASE_URL}${this.state.gameId}/check`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json; charset=utf-8" },
             body: JSON.stringify({ 
                 "row": row,
                 "col": col
@@ -58,30 +62,16 @@ class Minesweeper extends Component {
         })
         .then(resp => resp.json())
         .then(newGame => {
-            console.log(newGame)
-            this.setState({
-                game: newGame
-            })
-            if (this.state.game.state === "lost") {
-                console.log('you lost!')
-                this.stopTimer()
-            }
-            else if (this.state.game.state === "won") {
-                console.log('you won!')
-                this.stopTimer()
-            }
+            this.setState({ game: newGame })
+            this.displayGameResult()
         })
-        .catch(console.error)
-
     }
 
     flagBox = (e, row, col) => {
         e.preventDefault()
-        fetch(`${BASE_URL}/games/${this.state.gameId}/flag`, {
+        fetch(`${BASE_URL}${this.state.gameId}/flag`, {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-            },
+            headers: { "Content-Type": "application/json; charset=utf-8" },
             body: JSON.stringify({ 
                 "row": row,
                 "col": col
@@ -89,49 +79,41 @@ class Minesweeper extends Component {
         })
         .then(resp => resp.json())
         .then(newGame => {
-            console.log(newGame)
-            this.setState({
-                game: newGame
-            })
+            this.setState({ game: newGame })
         })
     }
 
-    renderCells = (row, col) => {
-        if (this.state.game.board[row][col] === "_") {
+    renderTiles = (row, col) => {
+        const tile = this.state.game.board[row][col]
+        if (tile === "_") {
             return "◻️"
         }
-        else if (this.state.game.board[row][col] === "F") {
+        else if (tile === "F") {
             return "🚩"
         }
-        else if (this.state.game.board[row][col] === "*") {
+        else if (tile === "*" || tile === "@") {
             return "💣"
         }
         else {
-            return this.state.game.board[row][col]
+            return tile
         }
     }
 
     render() {
         return (
-            <div className='board'>
-                <div className='title'>
-                    Minesweeper Game ID: #{this.state.game.id}
-                </div>
-                <div>
-                    It's been {this.state.timer} seconds since you started!
-                </div>
+            <div className='game'>
+                <div className='title'>Minesweeper Game ID: #{this.state.game.id}</div>
+                <div className='timer'>It's been {this.state.timer} seconds since you started!</div>
                 <div className='board-border'>
                     {this.state.game.board.map((row, i) => {
                         return (
                             <div key={i} className='row'>
                                 {row.map((col, j) => {
                                     return (
-                                        <span 
-                                        key={j} 
-                                        className='col' 
+                                        <span key={j} className='col' 
                                         onClick={() => this.checkBox(i, j)} 
                                         onContextMenu={(e) => this.flagBox(e, i, j)}>
-                                            {this.renderCells(i, j)}
+                                            {this.renderTiles(i, j)}
                                         </span>
                                     )
                                 })}
@@ -139,6 +121,7 @@ class Minesweeper extends Component {
                         )
                     })}
                 </div>
+                <div>{this.state.result}</div>
             </div>
         );
     }
